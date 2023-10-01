29cb448df0a89e886c6e04d5ff74d0f121ac227a
SaharMach <saharmach28@gmail.com>
2023-10-01 23:55:04 +0300

    check

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb+srv://sahar:sahar28@cluster0.wbgpl3g.mongodb.net/',
+    dbURL: 'mongodb+srv://sahar:sahar28@cluster0.jrft4z4.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
     dbName: 'toy_db',
 }
